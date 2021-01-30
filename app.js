7656064ae771265c882361714ebc09697dcb937f
Emiliano Tosar <emiliano.r.tosar@gmail.com>
2021-01-30 15:58:15 -0300

    add start script

--- app.js
+++ app.js
@@ -158,5 +158,6 @@ app.use((err, req, res, next) => {
 	res.status(statusCode).render('error', { err })
 })
 
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => console.log("Server listening!"));
+app.listen(port, () => console.log("Server listening!"));
